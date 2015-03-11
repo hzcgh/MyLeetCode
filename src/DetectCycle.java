569d98743f8363dda2cc13414ba72317319b521e
Peter Chen <hzcgh@live.cn>
2015-03-10 23:31:36 -0700

    Two puzzles

---
+++ src/DetectCycle.java
@@ -0,0 +1,53 @@
+/**
+ * Created by Peter on 3/10/2015.
+ */
+public class DetectCycle {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null)
+            return null;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        do{
+            if (fast.next!=null && fast.next.next!=null){
+                fast = fast.next.next;
+                slow = slow.next;
+            } else
+            {
+                return null;
+            }
+        } while(fast != slow);
+
+        ListNode cycle = head;
+        while(cycle!=slow){
+            slow = slow.next;
+            cycle = cycle.next;
+        }
+
+        return cycle;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+
+        n1.next = n2;
+        n2.next = n1;
+
+        DetectCycle d = new DetectCycle();
+        ListNode node = d.detectCycle(n1);
+
+        System.out.println(node.val);
+    }
+}
--- src/GenerateParenthesis.java
+++ src/GenerateParenthesis.java
@@ -34,38 +34,56 @@ public class GenerateParenthesis {
 
         return new ArrayList<>( map.get(n));*/
 
-        char[] str = new char[n*2];
+/*        char[] str = new char[n*2];
          ArrayList<String> list = new ArrayList<String>();
          addParen(list, n, n, str, 0);
-         return list;
+         return list;*/
+
+        List<String> ret = new ArrayList<>();
+        generate(n,n,"",ret);
+        return ret;
     }
 
-     public void addParen(ArrayList<String> list, int leftRem,
+/*     public void addParen(ArrayList<String> list, int leftRem,
                            int rightRem, char[] str, int count) {
          if (leftRem < 0 || rightRem < leftRem) return; // invalid state
 
-        if (leftRem == 0 && rightRem == 0) { /* no more parens left */
+        if (leftRem == 0 && rightRem == 0) { *//* no more parens left *//*
              String s = String.copyValueOf(str);
             list.add(s);
              } else {
-             /* Add left paren, if there are any left parens remaining. */
+             *//* Add left paren, if there are any left parens remaining. *//*
              if (leftRem > 0) {
                  str[count] = '(';
                  addParen(list, leftRem - 1, rightRem, str, count + 1);
                  }
 
-             /* Add right paren., if expression is valid */
+             *//* Add right paren., if expression is valid *//*
              if (rightRem > leftRem) {
                  str[count] = ')';
                  addParen(list, leftRem, rightRem - 1, str, count + 1);
                  }
              }
-         }
+         }*/
+
+    private void generate(int left, int right, String current, List<String> ret){
+        if (left==0 && right==0)
+        {
+            ret.add(current);
+            return;
+        }
+        if (left >0){
+            generate(left-1, right, current+"(", ret);
+        }
+        if (left<right){
+            generate(left, right-1, current+")",ret);
+        }
+    }
 
     public static void main(String[] args) {
-        long p = System.currentTimeMillis();
-        new GenerateParenthesis().generateParenthesis(14)/*.forEach(System.out::println)*/;
-        System.out.println((System.currentTimeMillis()-p));
+       // long p = System.currentTimeMillis();
+        new GenerateParenthesis().generateParenthesis(3).forEach(System.out::println);
+      //  System.out.println((System.currentTimeMillis()-p));
     }
 }
 
